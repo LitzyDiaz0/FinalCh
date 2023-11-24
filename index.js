017e4611befac10ab7daf877d38f010a0ef515d5
Litzy Diaz <108082744+LitzyDiaz0@users.noreply.github.com>
2023-11-23 19:33:01 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ function validar() {
    //  alert($("#user").val());
     var user = document.getElementById("user");
     var password = document.getElementById("password");
+    var bandera = false;
 
     alert("Tu usuario es: "+user.value+"   Contraseña: " +password.value);
+}
+
+function validar(){
+    if (bandera){
+        console.log("validacion completa")
+    }
+}
+
+function correctCaptcha(){
+    console.log("Captcha resuelto correctamente")
+    bandera = true;
 }
